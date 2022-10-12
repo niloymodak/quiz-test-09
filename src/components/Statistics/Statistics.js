290525c5d9aa630516f848a1ce940edafb7010e1
niloymodak <niloymodak5@gmail.com>
2022-10-12 17:21:02 +0600

    seventh commit

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis,Tooltip } from 'recharts';
-
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 const Statistics = () => {
     const quizs = useLoaderData();
     const quizItems = quizs.data;
@@ -15,23 +14,23 @@ const Statistics = () => {
     }
     return (
         <div>
-          <div className="container">
-          <div className="row mt-5">
-                <div className='col-md-6 offset-md-3 col-sm-12'>
+            <div className="container">
+                <div className="row mt-5">
+                    <div className='col-md-6 offset-md-3 col-sm-12'>
 
-                    <LineChart width={600} height={300} data={data}>
-                        <Line type="monotone" dataKey="total" stroke="#8884d8" />
-                        <CartesianGrid stroke="#ccc" />
-                        <XAxis dataKey="name" />
-                        <YAxis />
-                        <Tooltip />
-                    </LineChart>
+                        <LineChart width={600} height={300} data={data}>
+                            <Line type="monotone" dataKey="total" stroke="#8884d8" />
+                            <CartesianGrid stroke="#ccc" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                        </LineChart>
 
+                    </div>
+                    <h4 className="text-center mt-2 mb-5">Figure: Quiz Topics Chart</h4>
                 </div>
-                <h4 className="text-center mt-2 mb-5">Figure: Quiz Topics Chart</h4>
-            </div>
 
-          </div>
+            </div>
         </div>
     );
 };
