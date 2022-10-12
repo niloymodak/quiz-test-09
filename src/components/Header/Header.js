9cd1401c5ed536eb4fbebc1e079e68fe34ea8813
niloymodak <niloymodak5@gmail.com>
2022-10-12 16:52:55 +0600

    third commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import banner from '../../banner1.jpg';
 import './Header.css';
 
@@ -8,7 +8,7 @@ const Header = () => {
         <div>
             <nav className="navbar navbar-expand-lg">
                 <div className="container">
-                    <Link className="navbar-brand text-light" href="/">Quizo</Link>
+                    <Link className="navbar-brand text-light" href="/">Quiz-Test</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -29,10 +29,12 @@ const Header = () => {
                 </div>
             </nav>
             <div className='container banner mt-3'>
-               <img src={banner} className="w-100" alt="banner"></img>
+                <img src={banner} className="w-100" alt="banner"></img>
+                <br />
+                <h4>Web development is the work involved in developing a website for the Internet (World Wide Web) or an intranet (a private network).Web development can range from developing a simple single static page of plain text to complex web applications, electronic businesses, and social network services.</h4>
             </div>
         </div>
-       
+
     );
 };
 
